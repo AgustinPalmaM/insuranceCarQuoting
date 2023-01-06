e1a9a363583f253dc0e72de7bf81f5ec31d529b6
AgustinPalmaM <palma.agustin7@gmail.com>
2023-01-06 19:13:15 -0300

    Create showResultQuotingInsurance prototype to show a div with the value of the insurance

--- js/app.js
+++ js/app.js
@@ -6,6 +6,37 @@ function Insurance(brand, year, type) {
   this.type = type;
 }
 
+Insurance.prototype.quoteInsurance = function() {
+  
+  let quantity;
+  const baseAmount = 15000;
+
+  switch (this.brand) {
+    case '1':
+      quantity = baseAmount * 1.5;
+      break;
+    case '2':
+      quantity = baseAmount * 1.2;
+      break;
+    case '3':
+      quantity = baseAmount * 2.0;
+      break;  
+    default:
+      break;
+  }
+
+  // get the years of use of the selected car
+  const yearsOfUse = new Date().getFullYear() - this.year;
+  
+  // apply 3% of discount to quantity by every year of use
+  quantity -= (( yearsOfUse * 3 ) * quantity ) / 100;
+  
+  // apply 30% charge if type of insurance is basic or 50% if type of insurance is complete
+
+  this.type === 'basico' ? quantity *= 1.3 : quantity *= 1.5;
+  return quantity;
+  
+}
 function UI() {}
 
 UI.prototype.fillYearOptions = () => {
@@ -53,6 +84,36 @@ UI.prototype.showAlerts = (message, typeMessage) => {
 
 }
 
+UI.prototype.showResultQuotingInsurance = (insurance, totalInsurance) => {
+  const { brand, year, type } = insurance;
+  let brandText;
+  brand === '1' ? brandText = 'Americano' : brand === '2' ? brandText = 'Asiatico' : 'Europeo';
+
+  const result = document.querySelector('#resultado');
+  if(result.firstElementChild) {
+    result.firstElementChild.remove()
+  }
+  const div = document.createElement('DIV');
+  div.classList.add('mt-10');
+
+  div.innerHTML = `
+    <p class="header">Your summary<p>
+    <p class="font-bold">Brand: <span class="font-normal">${brandText}</span><p>
+    <p class="font-bold">Year: <span class="font-normal">${year}</span><p>
+    <p class="font-bold">Insurance Type: <span class="font-normal">${type}</span><p>
+    <p class="font-bold">Total: <span class="font-normal">$ ${totalInsurance}</span><p>
+  `;
+
+  
+  const spinner = document.querySelector('#cargando');
+  spinner.style.display = 'block'
+  
+  setTimeout(() => {
+    spinner.style.display = 'none'
+    result.appendChild(div);
+  }, 2000)
+}
+
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -85,7 +146,12 @@ function quoteInsurance(e) {
     return ui.showAlerts('there are empty fields', 'error')
   }
 
-  ui.showAlerts('quoting insurance...', 'exito')
+  ui.showAlerts('quoting insurance...', 'exito');
 
+  // instantiate the insurance with the data from the form
+  const insurance = new Insurance(brand, year, type);
+  const totalInsurance = insurance.quoteInsurance();
+
+  ui.showResultQuotingInsurance(insurance, totalInsurance);
 
 }
